942b4fd701e76caeee98dbf3c2cfb83b2cfd0e16
George Ken <georgenkenn@gmail.com>
2016-11-23 12:51:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,9 +20,11 @@ var nameInput=document.getElementById('name');
 name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
-  var names=['name1','name2','name3'];
+  var names=['name1','name2','name3','name4'];
   var list='';
   for(var i=0;i<names.length;i++){
-      
+    list+='<li>' + names[i] + '</li>' ; 
   }
+  var ul=documet.getElementById('namelist');
+  ul.innerHTML=list;
 };
