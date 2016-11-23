9e422b5c6633c6e55862a04c8751a993dc8297ca
George Ken <georgenkenn@gmail.com>
2016-11-23 13:29:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,8 +16,6 @@ var button=document.getElementById('counter');
    request.send(null);
  };
  
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
      var request=new XMLHttpRequest();
@@ -35,6 +33,9 @@ submit.onclick=function(){
      }    
     }  
    };
+   var nameInput=document.getElementById('name');
+   var name=nameInput.value;
+
    request.open('GET','http://gken96.imad.hasura-app.io/submit-name?name='+name,true);
    request.send(null);
  
