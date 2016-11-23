9e811b9ab0df89e00ea19f80fcbbbb7c594e4671
George Ken <georgenkenn@gmail.com>
2016-11-23 20:19:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,6 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var counter=0;
-app.get('/counter', function (req, res){
-   counter=counter+1;
-   res.send(counter.toString());
-});
 
 
 var articles = {
@@ -100,6 +95,13 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var counter=0;
+app.get('/counter', function (req, res){
+   counter=counter+1;
+   res.send(counter.toString());
+});
+
 
 var names=[];
 app.get('/submit-name',function(req,res){
